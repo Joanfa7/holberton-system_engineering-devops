e4e33cbf6ab457a7b5f827232922aecc888811a8
Joanfa7 <jafabre95@gmail.com>
2022-08-15 01:33:06 +0000

    adding new file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#Change to config file
+include  stdlib
+
+file_line { 'BatchMode':
+ensure => 'present',
+line   => 'PasswordAuthentication no',
+path   => '/etc/ssh/ssh_config',
+}
+
+file_line { 'IdentifyFile':
+ensure => 'present',
+line   => 'IdentityFile ~/.ssh/school',
+path   => '/etc/ssh/ssh_config',
+}
