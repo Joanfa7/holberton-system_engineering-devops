0e828dcc4de8c61edac6fefc19adcf1ac7c73235
Joanfa7 <jafabre95@gmail.com>
2022-10-06 17:13:28 +0000

    fixing puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,3 +1,5 @@
+# debuggint task 0
+
 exec {'ulimit-file':
   provider => shell,
   command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx && sudo service nginx restart'
